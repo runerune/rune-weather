2384183e4d7ce9a82ba38059332cb417f1afd75c
runerune <guildbot@bezkr.es>
2020-02-28 13:12:10 +0100

    stylowanie wykresu + grupowanie...

--- src/app/chart/chart.component.ts
+++ src/app/chart/chart.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit, Input, SimpleChanges } from '@angular/core';
+import { Component, OnInit, Input, SimpleChanges, ViewChild } from '@angular/core';
+import { BaseChartDirective } from 'ng2-charts';
 
 @Component({
 	selector: 'app-chart',
@@ -7,6 +8,7 @@ import { Component, OnInit, Input, SimpleChanges } from '@angular/core';
 })
 export class ChartComponent implements OnInit {
 	@Input() data;
+	@ViewChild('chart') chart: BaseChartDirective;
 
 	cData = [];
 	cLabels = [];
@@ -25,11 +27,50 @@ export class ChartComponent implements OnInit {
 				}
 			}],
 			yAxes: [{
+				display: false,
+				id: 'clouds',
+				//stacked: true,
 				ticks: {
-					min: 0,
+					beginAtZero: true,
+					max: 150,
+				},
+				type: 'linear',
+				position: 'right',
+			}, {
+				id: 'temperature',
+				type: 'linear',
+				scaleLabel: {
+					display: true,
+					labelString: '°C',
+				},
+				stacked: true,
+			}, {
+				id: 'wind',
+				type: 'linear',
+				gridLines: {
+					drawOnChartArea: false,
+				},
+				scaleLabel: {
+					display: true,
+					labelString: 'm/h',
+				},
+				stacked: true,
+			}, {
+				id: 'rain',
+				type: 'linear',
+				gridLines: {
+					drawOnChartArea: false,
+				},
+				position: 'right',
+				ticks: {
+					max: 3
+				},
+				scaleLabel: {
+					display: true,
+					labelString: 'mm/h',
 				}
 			}]
-		}
+		},
 	};
 	cLegend = true;
 	cType = 'line';
@@ -38,15 +79,48 @@ export class ChartComponent implements OnInit {
 		this.clear();
 	}
 
+	fillOptions = {
+		lineTension: .5,
+		borderWidth: 0,
+		borderColor: 'transparent',
+		pointRadius: 0,
+		yAxisID: 'clouds',
+	}
+
+	lineOptions = {
+		backgroundColor: 'transparent',
+		pointRadius: 0,
+	}
+
+	barOptions = {
+		type: 'bar',
+		stack: 'windRain',
+		barPercentage: 1,
+	}
+
 	clear(): void {
 		this.cData = [
-			{ data: [], label: 'Temperature' },
-			{ data: [], label: 'Wind' },
-			{ data: [], label: 'Rain', type: 'bar' },
-			{ data: [], label: 'Fog', type: 'bar' },
-			{ data: [], label: 'Clouds (low)' },
-			{ data: [], label: 'Clouds (medium)' },
-			{ data: [], label: 'Clouds (high)' },
+			{ data: [], label: 'Temperature', ...this.lineOptions, yAxisID: 'temperature', borderColor: '#00a8cc' },
+			{ data: [], label: 'Wind', yAxisID: 'wind', backgroundColor: '#d45079', ...this.barOptions },
+			{ data: [], label: 'Rain', backgroundColor: '#5fbdb0', yAxisID: 'rain', ...this.barOptions },
+			{
+				data: [],
+				label: 'Clouds (low)',
+				backgroundColor: 'rgba(108, 86, 123, .5)',
+				...this.fillOptions
+			},
+			{
+				data: [],
+				label: 'Clouds (medium)',
+				backgroundColor: 'rgba(192, 108, 132, .5)',
+				...this.fillOptions
+			},
+			{
+				data: [],
+				label: 'Clouds (high)',
+				backgroundColor: 'rgba(248, 177, 149, .5)',
+				...this.fillOptions
+			},
 		];
 		this.cLabels = [];
 	}
@@ -55,8 +129,17 @@ export class ChartComponent implements OnInit {
 		if(changes.data) {
 			this.clear();
 
-			let limit = 24;
+			let limit = 48;
 			let count = 0;
+
+			let minTemp = Infinity;
+
+			for(let i in changes.data.currentValue) {
+				let point = changes.data.currentValue[i];
+				if(point.temp < minTemp) minTemp = point.temp;
+			}
+
+			minTemp -= 1;
 
 			for(let i in changes.data.currentValue) {
 				if(count > limit) break;
@@ -67,14 +150,14 @@ export class ChartComponent implements OnInit {
 					hour: '2-digit',
 				}));
 
-				this.cData[0].data.push(point.temp);
-				this.cData[1].data.push(point.wind);
-				this.cData[2].data.push(point.rain*100);
-				//this.cData[3].data.push(point.fog);
-				this.cData[3].data.push(Math.random()*50);
-				this.cData[4].data.push(point.clouds.low);
-				this.cData[5].data.push(point.clouds.medium);
-				this.cData[6].data.push(point.clouds.high);
+				let changedTemp = point.temp-minTemp;
+
+				this.cData[0].data.push(changedTemp);
+				this.cData[1].data.push([point.wind, point.wind-0.03]);
+				this.cData[2].data.push(point.rain);
+				this.cData[3].data.push(point.clouds.low);
+				this.cData[4].data.push(point.clouds.medium);
+				this.cData[5].data.push(point.clouds.high);
 
 				count++;
 			}
